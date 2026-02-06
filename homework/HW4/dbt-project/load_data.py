42a14527bacb2a299fcc83433d214c5f16d03278
yehanlong <longyehan@hotmail.com>
2026-02-06 14:26:43 -0800

    delete unnecessary data

--- homework/HW4/dbt-project/load_data.py
+++ homework/HW4/dbt-project/load_data.py
@@ -6,7 +6,7 @@ con.execute("CREATE SCHEMA IF NOT EXISTS prod")
 for taxi_type in ["yellow", "green"]:
     con.execute(f"""
                 CREATE OR REPLACE TABLE prod.{taxi_type}_tripdata AS
-                SELECT * FROM read_parquet('/Users/ethandu/data-engineer-project/homework/HW4/{taxi_type}_tripdata_*.parquet', union_by_name=true)
+                SELECT * FROM read_parquet('{taxi_type}_tripdata_*.parquet', union_by_name=true)
             """)
 con.close()
 print ('Data loaded into DuckDB successfully.')
